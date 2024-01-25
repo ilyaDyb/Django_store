dbabd43fe814bd63be1b771148074160f2af0b7b
wicki <ilya77788899@mail.ru>
2024-01-25 18:37:14 +0300

    add debug_toolbar

--- app/urls.py
+++ app/urls.py
@@ -16,9 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from app.settings import DEBUG
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('main.urls', namespace='main')),
     path('catalog/', include('goods.urls', namespace='catalog'))
     ]
+
+if DEBUG:
+    urlpatterns += [
+    path("__debug__/", include("debug_toolbar.urls")),
+]
