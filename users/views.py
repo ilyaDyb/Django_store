cb2b9c3f97823b879889a08e2fffea11c7cfb212
wicki <ilya77788899@mail.ru>
2024-02-20 19:45:27 +0300

    notifications are ready

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
@@ -16,6 +16,7 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
+                messages.success(request, f"{username}, Вы вошли в аккаунт")
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
@@ -34,6 +35,8 @@ def registration(request):
             form.save()
             user = form.instance
             auth.login(request, user)
+            messages.success(request, f"{user.username}, Вы успешно зарегистрированы в аккаунт")
+
             return HttpResponseRedirect(reverse("main:index"))
             
     else:
@@ -51,6 +54,7 @@ def profile(request):
         form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
         if form.is_valid():
             form.save()
+            messages.success(request, "Профиль успешно обновлен")
             return HttpResponseRedirect(reverse("user:profile"))
             
     else:
@@ -65,5 +69,6 @@ def profile(request):
 
 @login_required
 def logout(request):
+    messages.success(request, f"{request.user.username}, Вы успешно вышли")
     auth.logout(request)
     return redirect(reverse("main:index"))
