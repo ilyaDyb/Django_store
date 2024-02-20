2705e24583a02036d0a1ca714df2dc610c13fafa
wicki <ilya77788899@mail.ru>
2024-02-20 19:12:45 +0300

    add profile's functions bugs fix

--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib import auth
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
 
 def login(request):
@@ -44,14 +45,25 @@ def registration(request):
     }
     return render(request, 'users/registration.html', context)
 
-
+@login_required
 def profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("user:profile"))
+            
+    else:
+        form = ProfileForm(instance=request.user)
+
     context = {
-        "title": "Home - Профиль"
+        "title": "Home - Регистрация",
+        "form": form
     }
     return render(request, 'users/profile.html', context)
 
 
+@login_required
 def logout(request):
     auth.logout(request)
     return redirect(reverse("main:index"))
